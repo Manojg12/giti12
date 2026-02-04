a6dac9682fb28166465de0e0f04697bdd6c39c9b
Manoj <manojg141203@gmail.com>
2026-02-04 10:31:05 +0530

    Verison 4

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,2 @@
-word = input("Enter your Word:")
-
-rev_word = word[::-1]
-
-if word == rev_word:
-	print(f"{word} is Palindrome")
-else: 
-	print(f"{word} is Not a Palindrome")
 
 # Palindrome Code
